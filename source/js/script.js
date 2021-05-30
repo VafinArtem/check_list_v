5087a202064e5f78ff7eac90e35c3a1a74bd0d70
Vafin Artem <a.vafin85@gmail.com>
2021-05-30 13:16:55 +0500

    добавил файл претиер, тестовый js и начал верстку

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,13 @@
 // Общий файл скриптов
+
+const userName = "James";
+
+const person = {first: userName};
+
+console.log(person);
+
+const sayHelloLinting = (fName) => {
+  console.log(`Hello linting, ${fName}`);
+};
+
+sayHelloLinting("James");
