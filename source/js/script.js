5962c79ccd9407d2fc194eb6ab84d5d35d716d1e
Vafin Artem <a.vafin85@gmail.com>
2021-05-30 13:32:31 +0500

    поправил scrip.js

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,1 @@
 // Общий файл скриптов
-
-const userName = "James";
-
-const person = {first: userName};
-
-console.log(person);
-
-const sayHelloLinting = (fName) => {
-  console.log(`Hello linting, ${fName}`);
-};
-
-sayHelloLinting("James");
